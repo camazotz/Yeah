6f2f63135af2d2041275ec942952f9dd57125758
camazotz <navalava@gmail.com>
2014-03-29 20:51:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,20 +4,20 @@ console.log('Whattaaaaaap');
 simply.on('singleClick', function(e) {
   console.log(util2.format('single clicked $button!', e));
   simply.vibe();
-  /*simply.on('accelData', function(e) {
+  simply.on('accelData', function(e) {
   	simply.body(e.accel.x + "  " + e.accel.y);
-  	
+  	/*
   	if (e.accel.x > 200 && e.accel.y > 200)
   	{
   	  simply.subtitle('It is positive');
-  	}
+  	}*/
   	
   	//simply.subtitle('Tapped' + e.axis);
   	if (e.axis === 'y')
   	{
   		simply.subtitle('Success!!');
   	}
-  })*/
+  })
   simply.subtitle('Pressed ' + e.button + '!');
 });
 
